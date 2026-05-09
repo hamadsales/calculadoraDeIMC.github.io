295e7be5f73d71672994d60d43818f3239d94e88
Hamad Sales <hamadsales@gmail.com>
2026-05-09 20:02:21 -0300

    Adicionando objetos ao JS

---
+++ script.js
@@ -0,0 +1,6 @@
+var altura = window.document.querySelector('input#altura')
+var idade = window.document.querySelector('input#idade')
+var peso = window.document.querySelector('input#peso')
+//Dessa forma podemos acessar o radio selecionado pelo mesmo name
+var radioSelecionado = window.document.querySelector('input[name="genero"]:checked')
+
