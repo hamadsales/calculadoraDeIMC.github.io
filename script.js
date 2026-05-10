0d2648a5dd3e2e62c3e8f716251c08312ce56db1
Hamad Sales <hamadsales@gmail.com>
2026-05-09 23:18:28 -0300

    "Projeto finalizado"

--- script.js
+++ script.js
@@ -11,18 +11,27 @@ dataAtual.innerHTML = `Agora são ${hora}h${minutos}m`
 
 function CalculaIMC() {
     var imc = (Number(peso.value) / (Number(altura.value) ** 2)).toFixed(2)
-    res.innerHTML = `O seu IMC é ${imc}`
-    if(imc<18.5){
-        res.innerHTML = `O seu imc é ${imc} e você está com Peso Baixo`
-    } else if(imc>=18.5 && imc<=24.9){
-        res.innerHTML = `O seu imc é ${imc} e você está com Peso Normal`
-    }else if(imc>=25 && imc<=29.9){
-        res.innerHTML = `O seu imc é ${imc} e você está com Sobrepeso`
-    }else if(imc>=30 && imc<=34.9){
-        res.innerHTML = `O seu imc é ${imc} e você está com Obesidade I`
-    }else if(imc>=35 && imc<=39.9){
-        res.innerHTML = `O seu imc é ${imc} e você está com Obesidade II`
-    }else if(imc>=40){
-        res.innerHTML = `O seu imc é ${imc} e você está com Obesidade III, busque ajuda imediatamente`
+    
+    // Limpa as classes de cores anteriores antes de aplicar a nova
+    res.className = "";
+
+    if(imc < 18.5){
+        res.innerHTML = `Seu IMC é ${imc}: Peso Baixo`
+        res.classList.add("amarelo")
+    } else if(imc >= 18.5 && imc <= 24.9){
+        res.innerHTML = `Seu IMC é ${imc}: Peso Normal`
+        res.classList.add("verde")
+    } else if(imc >= 25 && imc <= 29.9){
+        res.innerHTML = `Seu IMC é ${imc}: Sobrepeso`
+        res.classList.add("amarelo")
+    } else if(imc >= 30 && imc <= 34.9){
+        res.innerHTML = `Seu IMC é ${imc}: Obesidade I`
+        res.classList.add("laranja")
+    } else if(imc >= 35 && imc <= 39.9){
+        res.innerHTML = `Seu IMC é ${imc}: Obesidade II`
+        res.classList.add("vermelho-claro")
+    } else if(imc >= 40){
+        res.innerHTML = `Seu IMC é ${imc}: Obesidade III`
+        res.classList.add("vermelho-forte")
     }
 }
